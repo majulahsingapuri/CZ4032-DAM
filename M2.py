c6234b0da282f2bb046fb093d6c5c1c457b601e2
zhang zhuyan <zhangzhuyan53@gmail.com>
2021-09-13 23:50:04 +0800

    satisfiable done

--- M2.py
+++ M2.py
@@ -1,7 +1,22 @@
-'''
-Ruleitems class
-class_label 
-condset
 
 
-'''
+
+def isRuleInDataCase(datacase,rule):
+    # check if the individual data case's attribute and value the same as the precedence in rule
+    for item in rule:
+        if (datacase[item]!=rule[item]):
+            return False
+    return True
+def isCorrect(datacase,rule):
+    # assumption: rule must be in datacase first 
+    # return if the rule correctly classify the datacase d.
+    if datacase[-1]==rule.class_label:
+        return True
+    else:
+        return False
+
+class M2_classifier(CARs,dataset):
+    sorted(CARs)
+    
+
+
---
+++ readme.md
@@ -0,0 +1,17 @@
+# class 
+
+@total_ordering    // https://www.geeksforgeeks.org/sorting-objects-of-user-defined-class-in-python/ 
+Ruleitems
+{
+class_label 
+condset : dictionary of key value pairs
+Out of less than(__lt__), greater than(__gt__), less than equal to(__le__) and greater than equal to(__ge__), atleast one should be defined in the class being decorated using this method.
+}
+
+so that can use (sorted(cars))
+
+
+
+the last element in case d is the class label 
+
+dataset is a list of list  [[1, 1, 1], [1, 1, 1], [1, 2, 1], [2, 2, 1], [2, 2, 1]]
